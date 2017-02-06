3aecf21a99bf6a4c4f94f43b1cb6fd98c5e4e5c1
nikita <nikita14021997@gmail.com>
2017-02-06 02:18:48 -0800

    added news

--- send_flowers/static/js/my.js
+++ send_flowers/static/js/my.js
@@ -1,1 +1,12 @@
-#my js
+$(document).ready(function() {
+var movementStrength = 25;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("#item-img layer").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 25;
+          var newvalueY = height * pageY * -1 - 50;
+          $('#item-img layer').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
